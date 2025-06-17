fb435d9b4fcc5d44123ec00f83516925515c1e78
Niall Whelan <200600787@student.georgianc.on.ca>
2025-06-17 12:14:59 -0400

    Add files via upload

---
+++ files/StudentViewController.java
@@ -0,0 +1,56 @@
+package exam;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+
+public class StudentViewController  {
+
+    @FXML
+    private TableView<Student> tableView;
+
+    @FXML
+    private TableColumn<Student, Integer> studentNumCol;
+
+    @FXML
+    private TableColumn<Student, String> firstNameCol;
+
+    @FXML
+    private TableColumn<Student, String> lastNameCol;
+
+    @FXML
+    private TableColumn<Student, String> telephoneCol;
+
+    @FXML
+    private TableColumn<Student, String> addressCol;
+
+    @FXML
+    private TableColumn<Student, String> provinceCol;
+
+    @FXML
+    private TableColumn<Student, Integer> avgGradeCol;
+
+    @FXML
+    private TableColumn<Student, String> majorCol;
+
+    @FXML
+    private CheckBox ontarioCheckBox;
+
+    @FXML
+    private Label numOfStudentsLabel;
+
+    @FXML
+    private CheckBox honourRollCheckBox;
+
+    @FXML
+    private ComboBox<String> areaCodeComboBox;
+
+    @FXML
+    private void applyFilter()  {
+    }
+
+    @FXML
+    void initialize() {
+        areaCodeComboBox.getItems().add("All");
+    }
+}
