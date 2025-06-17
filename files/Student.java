da054ddb0dd6197b0ac11a90799e2a8b2ff18102
niall-whelan <200600787@student.georgianc.on.ca>
2025-06-17 13:26:16 -0400

    May have fixed the sql problem but definetly not the sdk problem. idk why my sdk freaks out when i dont make the files

--- files/Student.java
+++ files/Student.java
@@ -5,4 +5,9 @@ public class Student {
     //cant get into openvpn
     //im actually screwed
     //my sdk wont set up properly
+    //no matter what sdk i try it wont register
+    //my computer does not like files from other computers
+    //my computer wont let me access the sql database
+    //im quite uncertain of what to do
+    //fixed it by probably the worst way possible
 }
